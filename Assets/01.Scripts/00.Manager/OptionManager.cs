a4af6894b27c426d221b4c07a7b48e7d46bafef4
191012 <kimhj191012@naver.com>
2026-04-06 12:16:09 +0900

    feat : GameOut UI

--- Assets/01.Scripts/00.Manager/OptionManager.cs
+++ Assets/01.Scripts/00.Manager/OptionManager.cs
@@ -6,25 +6,34 @@ namespace _01.Scripts._00.Manager
     public class OptionManager : SingletonObject<OptionManager>
     {
         [SerializeField] private GameObject optionPanel;
+        [SerializeField] private GameObject outPanel;
 
         protected override void Awake()
         {
             base.Awake();
             
             optionPanel.SetActive(false);
+            outPanel.SetActive(false);
         }
 
-        private void Start()
-        {
-            InputManager.AddListener(ActionCode.Option, InputType.Down, () =>
-            {
-                optionPanel.SetActive(!optionPanel.activeSelf);
-            });
-        }
-
-        public void OnClickButton()
+        public void OnClickOptionButton()
         {
             optionPanel.SetActive(!optionPanel.activeSelf);
+        }
+
+        public void OnClickOutYesButton()
+        {
+            // todo : Game Save
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+
+        public void OnClickOutNoButton()
+        {
+            outPanel.SetActive(!outPanel.activeSelf);
         }
     }
 }
