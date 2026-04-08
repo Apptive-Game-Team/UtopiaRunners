40543406839270a51d4fe4969141e726e5746594
RedPanda <jdkyu44@naver.com>
2026-04-08 19:57:39 +0900

    player controller

---
+++ Assets/01.Scripts/07.Character/PlayerController.cs
@@ -0,0 +1,75 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D rb;
+    private PlayerInput input;
+
+    [Header("Jump")]
+    public float jumpForce = 10f;
+    public int maxJumpCount = 2;
+    public int jumpCount;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float checkDistance = 0.1f;
+    public LayerMask groundLayer;
+
+    [Header("Status")]
+    public bool isGrounded;
+    public bool isSliding;
+    public bool isDead;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        input = GetComponent<PlayerInput>();
+        jumpCount = maxJumpCount;
+    }
+
+    void Update()
+    {
+        CheckGround();
+        CheckLanding();
+        HandleInput();
+    }
+
+    public void Jump()
+    {
+        if (jumpCount == 0) return;
+        //if (isDead) return;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        jumpCount--;
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.Raycast(
+            groundCheck.position,
+            Vector2.down,
+            checkDistance,
+            groundLayer
+        );
+    }
+
+    private void CheckLanding()
+    {
+        if (rb.linearVelocityY > 0.01f) return;
+
+        if (isGrounded)
+            jumpCount = maxJumpCount;
+    }
+
+    void HandleInput()
+    {
+        //if (isDead) return;
+
+        if (input.jumpPressed)
+            Jump();
+    }
+}
---
+++ Assets/01.Scripts/07.Character/PlayerInput.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public bool jumpPressed;
+    public bool slidePressed;
+    public bool skillPressed;
+    public bool tagPressed;
+
+    private void Update()
+    {
+        jumpPressed = Input.GetKeyDown(KeyCode.Space);
+        slidePressed = Input.GetKeyDown(KeyCode.DownArrow);
+        skillPressed = Input.GetKeyDown(KeyCode.E);
+        tagPressed = Input.GetKeyDown(KeyCode.Q);
+    }
+}
